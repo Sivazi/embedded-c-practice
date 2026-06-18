384635d236e35f1df22be9d181795c5b06b18a33
Sivazi <sivazipeddakota@gmail.com>
2026-06-18 18:17:51 +0530

    set-and-clear-bit-counter

---
+++ bit manipulation/set-clear-bit-counter.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int num,m=0,n=0;
+    printf("enter a number:\n");
+    scanf("%d",&num);
+
+    int pos=31;
+   
+  
+l:
+    printf("%d",num>>pos&1);
+    if((num>>pos&1)==1)
+    m++;
+   else
+    n++;
+    pos--;
+    if(pos>=0)
+        goto l;
+    printf("\n");
+    printf("the no of set bits = %d\n",m);
+    printf("the no of clear bits = %d",n);
+}
